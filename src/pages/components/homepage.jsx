3509ea5a35997f4e0161a22677791d4362706a04
chasacademy-EmmaJohansson <114146126+chasacademy-EmmaJohansson@users.noreply.github.com>
2023-04-26 12:35:35 +0200

    changes

---
+++ src/pages/components/homepage.jsx
@@ -0,0 +1,8 @@
+export default function Homepage() {
+  return (
+    <div>
+      Hej
+      <h1>Blog Bitch</h1>
+    </div>
+  );
+}
---
+++ src/pages/components/navbar.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="border-b-2 border-gray-800 p-4">
+      <ul className="flex flex-col md:flex-row md:justify-evenly">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="components/events">Events</Link>
+        </li>
+        <li>
+          <Link href="components/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
