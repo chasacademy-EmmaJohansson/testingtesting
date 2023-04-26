b35a956184ede567d260ca5a7a43ee04f79d157a
Aran Asaiesh <aran.asaiesh@chasacademy.se>
2023-04-26 12:42:37 +0200

    aboutpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Navbar from "./components/navbar";
 import { Inter } from "next/font/google";
 import Homepage from "./components/homepage";
+import About from "./about";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+      <About />
       <Homepage />
     </main>
   );
